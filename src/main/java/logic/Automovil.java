5e68c29f8c3eca684c0c468305b5e2aec64b2ca5
Matias Ambrosi <matiambrosi@gmail.com>
2022-12-21 04:18:15 -0300

    Inicializacion y creada la clase Automovil

---
+++ src/main/java/logic/Automovil.java
@@ -0,0 +1,83 @@
+
+package logic;
+
+public class Automovil {
+    private int id;
+    private String modelo;
+    private String marca;
+    private String motor;
+    private String color;
+    private String patente;
+    private String cantidadPuertas;
+
+    public Automovil() {
+    }
+
+    public Automovil(int id, String modelo, String marca, String motor, String color, String patente, String cantidadPuertas) {
+        this.id = id;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.motor = motor;
+        this.color = color;
+        this.patente = patente;
+        this.cantidadPuertas = cantidadPuertas;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public String getCantidadPuertas() {
+        return cantidadPuertas;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public void setCantidadPuertas(String cantidadPuertas) {
+        this.cantidadPuertas = cantidadPuertas;
+    }
+    
+    
+}
