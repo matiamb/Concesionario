b6dfe364c885dfe444c35c4736d02e578e0734b8
Matias Ambrosi <matiambrosi@gmail.com>
2023-01-16 01:38:28 -0300

    Jpa controller creado

--- src/main/java/logic/Automovil.java
+++ src/main/java/logic/Automovil.java
@@ -1,13 +1,14 @@
 
 package logic;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Automovil {
+public class Automovil implements Serializable {
     @Id
     @GeneratedValue (strategy = GenerationType.SEQUENCE )
     private int id;
